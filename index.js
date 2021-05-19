992059aa1a7547edccdc6f5210e689c89b3b4583
Daniel Mueller <danielpetermueller@gmail.com>
2021-05-19 12:33:25 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-
+function superbowlWin(records){
+    let win = records.find(record => record.result === 'W');
+    if (win){
+        return win.year;
+    } 
+    // if (record.result === 'W')
+    // return year;
+// console.log('myRecord',record)
+}
+// record.find(superbowlWin);
